b6617c4a5768449b34895bc41a94fbd656efd495
David Hullar <hullardavid@gmail.com>
2018-12-19 16:43:01 +0100

    Basic structure for solving Day6

--- include/day6.h
+++ include/day6.h
@@ -8,21 +8,61 @@
 
 // global variables/types
 
-const unsigned int GL_NUM {50};
+const unsigned int GL_NUM {6};
 
 struct Point
 {
     int x, y;
+    Point(int x = 0, int y = 0) : x(x), y(y) {}
+
+    friend std::ostream& operator<<(std::ostream &out, const Point &p);
+    friend bool operator==(const Point &p1, const Point &p2);
+};
+
+struct Rectangle
+{
+    Point p1, p2;
+    Rectangle(Point &p1, Point &p2) : p1(p1), p2(p2) {}
+
+    friend std::ostream& operator<<(std::ostream &out, const Rectangle &r);
+};
+
+class Grid
+{
+private:
+    int offset_x, offset_y;
+    int width, height;
+
+    Point *points;
+    void initGrid();
+
+public:
+    Grid(Point &p1, Point &p2) : offset_x(p1.x), offset_y(p1.y),
+        width(p2.x - p1.x + 1), height(p2.y - p1.y + 1)
+    {
+        points = new Point[width * height];
+
+        initGrid();
+    }
+
+    int size() { return width * height; }
+    Point& operator[](int idx) { return points[idx]; }
+
+    void draw();
 };
 
 // function prototypes
 
 void solveDay6();
 
-std::array<Point, GL_NUM> fillInCoords();
+//std::array<Point, GL_NUM> fillInCoords();
 
-int getLargestArea(std::array<Point, GL_NUM> &coords);
+std::array<Point, GL_NUM> fillInCoordsTest();
 
-void printCoords(std::array<Point, GL_NUM> &coords);
+int getLargestArea(const std::array<Point, GL_NUM> &coords);
+
+Rectangle getSeekRect(const std::array<Point, GL_NUM> &coords);
+
+void printCoords(const std::array<Point, GL_NUM> &coords);
 
 #endif DAY6_H
--- src/day6.cpp
+++ src/day6.cpp
@@ -1,27 +1,85 @@
 #include "day6.h"
 
+
 void solveDay6()
 {
-    std::array<Point, GL_NUM> coords = fillInCoords();
+    std::array<Point, GL_NUM> coords = fillInCoordsTest();
 
     printCoords(coords);
+
+    getLargestArea(coords);
 }
 
 
-int getLargestArea(std::array<Point, GL_NUM> &coords)
+int getLargestArea(const std::array<Point, GL_NUM> &coords)
 {
+    // the coordinates outside of the seek-rectangle are irrelevant because they are sure to have infinite area
+    Rectangle seekRect = getSeekRect(coords);
+
+    Grid grid(seekRect.p1, seekRect.p2);
+
+    grid.draw();
+
+    std::cout << seekRect << std::endl;
 
     return 1;
 }
 
-
-
-void printCoords(std::array<Point, GL_NUM> &coords)
+// the seekmap is defined by the four boundary points: get the left/up and right/down corners
+Rectangle getSeekRect(const std::array<Point, GL_NUM> &coords)
 {
+    Point P_start = coords[0];
+    Point P_end = P_start;
+
     for (const auto &coord : coords)
-        std::cout << coord.x << ", " << coord.y << '\n';
+    {
+        // find the left/up corner
+        if (coord.x < P_start.x)
+            P_start.x = coord.x;
+        if (coord.y < P_start.y)
+            P_start.y = coord.y;
+
+        // find the right/down corner
+        if (coord.x > P_end.x)
+            P_end.x = coord.x;
+        if (coord.y > P_end.y)
+            P_end.y = coord.y;
+    }
+
+    return Rectangle(P_start, P_end);
 }
 
+
+void printCoords(const std::array<Point, GL_NUM> &coords)
+{
+    for (const auto &coord : coords)
+        std::cout << coord << '\n';
+
+    std::cout << std::endl;
+}
+
+
+std::ostream& operator<<(std::ostream &out, const Point &p)
+{
+    out << "(" << p.x << ", " << p.y << ")";
+
+    return out;
+}
+
+
+std::ostream& operator<<(std::ostream &out, const Rectangle &r)
+{
+    out << r.p1 << '\n' << r.p2;
+
+    return out;
+}
+
+bool operator==(const Point &p1, const Point &p2)
+{
+    return (p1.x == p2.x && p1.y == p2.y);
+}
+
+/*
 std::array<Point, GL_NUM> fillInCoords()
 {
     std::array<Point, GL_NUM> coords {{
@@ -78,4 +136,42 @@ std::array<Point, GL_NUM> fillInCoords()
     }};
 
     return coords;
+}
+*/
+
+std::array<Point, GL_NUM> fillInCoordsTest()
+{
+    std::array<Point, GL_NUM> coords {{
+        {1, 1},
+        {1, 6},
+        {8, 3},
+        {3, 4},
+        {5, 5},
+        {8, 9}
+    }};
+
+    return coords;
+}
+
+void Grid::initGrid()
+{
+    for (int idx {0}; idx < size(); ++idx)
+    {
+        points[idx].x = offset_x + (idx % width);
+        points[idx].y = offset_y + static_cast<int>(idx / width);
+    }
+}
+
+
+void Grid::draw()
+{
+    for (int idx {0}; idx < size(); ++idx)
+    {
+        std::cout << points[idx] << ' ';
+
+        if ( (idx + 1) % width == 0 && idx > 0)
+            std:: cout << '\n';
+    }
+
+    std::cout << std::endl;
 }
